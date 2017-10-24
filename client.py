5bd746a2b9bf6db84dfba05b5822a62f1b2c4134
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-23 17:13:52 -0700

    servers connected

--- client.py
+++ client.py
@@ -1,1 +1,19 @@
 """."""
+
+import sys
+import socket
+
+
+def client(message):
+    """."""
+    # open socket connection to server
+    # send message var to server through socket
+    # accumulate any reply into a string
+    # once full reply is recieved close socket and return the mesasge
+    infos = socket.getaddrinfo('127.0.0.1', 5555)
+    stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream_info[:3])
+    client.connect(stream_info[-1])
+
+if __name__ == '__main__':
+    client(sys.argv[1])
--- server.py
+++ server.py
@@ -1,1 +1,24 @@
 """."""
+
+import sys
+import socket
+
+
+def server():
+    """."""
+    server = socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM,
+                           socket.IPPROTO_TCP)
+    address = ('127.0.0.1', 5555)
+    server.bind(address)
+    server.listen(1)
+    conn, addr = server.accept()
+
+
+if __name__ == '__main__':
+    try:
+        server()
+    except KeyboardInterrupt:
+        # close all open sockets
+        print("\nGoodbye")
+        sys.exit()
