cf43c23c6932d11645d607d0a151b81bb7a578f5
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-10-27 16:52:56 -0700

    passing tests

--- test_servers.py
+++ test_servers.py
@@ -4,6 +4,13 @@
 from __future__ import unicode_literals
 import pytest
 
+BAD_REQUEST_TABLE = [('not an http request', ('400', 'Bad Request')),
+                     ("/path/to/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>", ('400', 'Bad Request')),
+                     ("GET /path/to/index.html HTTP/1.0<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>", ('400', 'Bad Request'))]
+
+NOT_FOUND_TABLE = [("GET /not/a/real/file/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>", ('404', 'Not Found')),
+                   ("GET /fake/path/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>", ('404', 'Not Found')),
+                   ("GET /another/bad/path/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>", ('404', 'Not Found'))]
 
 PARSE_TABLE = [('GET /path/to/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>', '/path/to/index.html'),
                ('GET /zach/is/cool.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>', '/zach/is/cool.html')]
@@ -14,18 +21,20 @@ PARSE_FAIL_TABLE = [('/path/to/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80
                     ('GET /path/to/index.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF>')]
 
 
-def test_bad_request_error():
+@pytest.mark.parametrize('n, result', BAD_REQUEST_TABLE)
+def test_bad_request_error(n, result):
     """Test invalid http request returns response error."""
     from client import client
     from server import response_error
-    assert client('not an http request') == response_error('400', 'Bad Request')
+    assert client(n) == response_error(*result)
 
 
-def test_not_found_error():
+@pytest.mark.parametrize('n, result', NOT_FOUND_TABLE)
+def test_not_found_error(n, result):
     """Test invalid http request returns response error."""
     from client import client
     from server import response_error
-    assert client('GET not_a_webpage.html HTTP/1.1<CRLF>Host: www.mysite1.com:80<CRLF><CRLF>') == response_error('404', 'Not Found')
+    assert client(n) == response_error(*result)
 
 
 @pytest.mark.parametrize('n, result', PARSE_TABLE)
